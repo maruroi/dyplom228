eb144952f5de615492669272fc9293ac09aacea0
maruroi <morinmaksim9@gmail.com>
2025-05-24 12:17:32 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -139,32 +139,6 @@ async function submitQuestionForm(event) {
     }
 }
 
-document.getElementById('contactForm').addEventListener('submit', async function(event) {
-    event.preventDefault();
-
-    const formData = new FormData(this);
-    const data = Object.fromEntries(formData.entries());
-
-    try {
-        const response = await fetch('http://localhost:3000/api/messages', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(data)
-    });
-
-    if (response.ok) {
-        alert('Message sent successfully!');
-        this.reset();
-    } else {
-        alert('Failed to send message.');
-    }
-
-    } catch (error) {
-        console.error('Error submitting form:', error);
-        alert('An error occurred.');
-    }
-});
-
 // document.getElementById('contactForm').addEventListener('submit', async function(event) {
 //     event.preventDefault();
 
@@ -172,24 +146,50 @@ document.getElementById('contactForm').addEventListener('submit', async function
 //     const data = Object.fromEntries(formData.entries());
 
 //     try {
-//         const response = await fetch('https://nexgen-backend.onrender.com/api/messages', {
+//         const response = await fetch('http://localhost:3000/api/messages', {
 //             method: 'POST',
 //             headers: { 'Content-Type': 'application/json' },
 //             body: JSON.stringify(data)
-//         });
+//     });
 
-//         if (response.ok) {
-//             alert('Message sent successfully!');
-//             this.reset();
-//         } else {
-//             alert('Failed to send message.');
-//         }
+//     if (response.ok) {
+//         alert('Message sent successfully!');
+//         this.reset();
+//     } else {
+//         alert('Failed to send message.');
+//     }
 
 //     } catch (error) {
 //         console.error('Error submitting form:', error);
 //         alert('An error occurred.');
 //     }
 // });
+
+document.getElementById('contactForm').addEventListener('submit', async function(event) {
+    event.preventDefault();
+
+    const formData = new FormData(this);
+    const data = Object.fromEntries(formData.entries());
+
+    try {
+        const response = await fetch('https://nexgen-backend.onrender.com/api/messages', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data)
+        });
+
+        if (response.ok) {
+            alert('Message sent successfully!');
+            this.reset();
+        } else {
+            alert('Failed to send message.');
+        }
+
+    } catch (error) {
+        console.error('Error submitting form:', error);
+        alert('An error occurred.');
+    }
+});
 
 
 window.addEventListener("scroll", function () {
